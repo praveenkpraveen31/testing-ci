2d49d2ddb5ec1060fc4bc994179095d879270b31
praveenkpraveen31 <praveenkpraveen31@gmail.com>
2026-05-29 19:53:27 +0530

    mocha test

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,5 +1,8 @@
+const { expect } = require('chai');
 const add = require('../app');
 
-test('adds 2 + 3 to equal 5', () => {
-  expect(add(2, 3)).toBe(5);
+describe('Addition Test', () => {
+  it('should return 5 when adding 2 and 3', () => {
+    expect(add(2, 3)).to.equal(5);
+  });
 });
